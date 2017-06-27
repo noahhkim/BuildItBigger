3ee24e53fd83e25f77c31cf092b4ea1b4cdb656d
noahhkim <noahkim03@gmail.com>
2017-06-26 23:30:13 -0700

    feat: Add a Java library
    Create a new Gradle Java project and introduce a project dependency between the app and the Java library. The button displays a toast showing a joke retrieved from the Java joke telling library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeSource;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeSource jokeSource = new JokeSource();
+        String lameJoke = jokeSource.tellAJoke();
+        Toast.makeText(this, lameJoke, Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokesource/src/main/java/com/example/JokeSource.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class JokeSource {
+
+    public String tellAJoke() {
+        return "What kind of bagel can fly? A plain bagel";
+    }
+}
