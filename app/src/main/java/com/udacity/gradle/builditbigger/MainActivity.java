2b6bb1e945b7a3b4cdcc9b4896496aff056bf678
noahkim <noahkim03@gmail.com>
2017-07-10 15:55:09 -0700

    fix: Product flavor import in MainActivity
    Change the package directory in both free and paid MainActivityFragment so that MainActivity doesn't have to import each class when switching between each flavor

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,8 +7,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import com.udacity.gradle.builditbigger.paid.MainActivityFragment;
-
 
 public class MainActivity extends AppCompatActivity {
 
