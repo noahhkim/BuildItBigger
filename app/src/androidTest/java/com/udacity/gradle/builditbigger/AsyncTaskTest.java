8c227ed0a3d475f2dabcd053d52b3d66056f33f4
noahkim <noahkim03@gmail.com>
2017-07-10 16:56:09 -0700

    test: Check that AsyncTaskTest retrieves a non-empty string
    Fix the AsyncTaskTest to make sure that the string returned from AsyncTask is not null

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -1,31 +1,55 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.ExecutionException;
+
+import timber.log.Timber;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by Noah on 6/28/2017.
  */
 @RunWith(AndroidJUnit4.class)
 public class AsyncTaskTest {
+    private Context mContext;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule<>(MainActivity.class);
 
+    @Before
+    public void setUp() {
+        mContext = getInstrumentation().getTargetContext();
+    }
     @Test
     public void nonEmptyStringTest() {
-        onView(withId(R.id.tell_joke_button)).perform(click());
-        onView(withId(R.id.joke_textview)).check(matches(isDisplayed()));
+        Timber.d("AsyncTaskTest");
+        String joke = null;
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(mContext, null);
+        endpointsAsyncTask.execute();
+        try {
+            joke = endpointsAsyncTask.get();
+            Timber.d("Retrieved joke: " + joke);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(joke);
     }
 }
